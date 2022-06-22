b4627864ee37a36e71a277eec6b7b76e90fb050b
Krutika-Muley <91215900+Krutika-Muley@users.noreply.github.com>
2022-06-23 00:48:13 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,23 +1,48 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
-namespace MisalignedSpace {
-    class Misaligned {
-        static int printColorMap() {
-            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
-            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
+namespace MisalignedSpace
+{
+    class Misaligned
+    {
+        static string formatColorMapManual()
+        {
+            string formattedString = "";
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
             int i = 0, j = 0;
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    formattedString += $"{i *5 + j} | {majorColors[i]} | {minorColors[i]}\n";
                 }
             }
-            return i * j;
+            return formattedString;
         }
-        static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
+        static void printOnConsole(string formattedString)
+        {
+            Console.WriteLine(formattedString);
+        }
+        static void Main(string[] args)
+        {
+            string formattedString = formatColorMapManual();
+            printOnConsole(formattedString);
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+            string expectedString = "";
+            int i = 0, j = 0;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    expectedString += $"{i * 5 + j + 1} | {majorColors[i]} {minorColors[j]}\n";
+                }
+            }
+            printOnConsole(expectedString);
+            Debug.Assert(formattedString.Equals(expectedString));
             Console.WriteLine("All is well (maybe!)");
+            Console.ReadLine();
         }
     }
 }
